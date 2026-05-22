684bebefd537f828806a1c0b08686141ee9aef1c
Arya Gopal <arya@Aryas-MacBook-Air.local>
2026-05-22 03:26:43 -0700

    test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ export class AuthController {
 
   @Post('login')
   async login(@Body() body: { email: string; password: string }) {
+    console.log("controller")
     return this.authService.login(body.email, body.password);
   }
   @Post('msg')
