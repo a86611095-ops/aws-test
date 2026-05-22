dd7678a992a9d57e3287b35818e2084e18d29453
Arya Gopal <arya@Aryas-MacBook-Air.local>
2026-05-21 23:40:55 -0700

    added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,4 +9,8 @@ export class AuthController {
   async login(@Body() body: { email: string; password: string }) {
     return this.authService.login(body.email, body.password);
   }
+  @Post('msg')
+  async queue() {
+    return this.authService.pollMessage();
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,101 @@
+import { InternalErrorException, PublishCommand, SNSClient } from "@aws-sdk/client-sns";
+import { DeleteMessageCommand, ReceiveMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
 import { BadRequestException, Injectable } from "@nestjs/common";
+import dotenv from "dotenv";
+import {
+  CognitoIdentityProviderClient,
 
+  InitiateAuthCommand,
+} from "@aws-sdk/client-cognito-identity-provider";
+dotenv.config();
+
+const sns = new SNSClient({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET!,
+  }
+});
+
+const sqs=new SQSClient({   region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET!,
+  }
+})
+  const queue="https://sqs.us-east-1.amazonaws.com/389548782125/user-signup-queue"
+
+const topic="arn:aws:sns:us-east-1:389548782125:user-event-topic"
 @Injectable()
-export class AuthService {
 
+export class AuthService {
+  private client = new CognitoIdentityProviderClient({
+    region: "eu-north-1",
+  });
 
   async login(email: string, password: string) {
+     const command = new InitiateAuthCommand({
+      AuthFlow: "USER_PASSWORD_AUTH",
+      ClientId: "7g56s0u9h6glmnpcgahno5et2n",
+      AuthParameters: {
+        USERNAME: email,
+        PASSWORD: password,
+      },
+    });
+
+    const response = await this.client.send(command);
+    console.log(response, "rees");
+        await this.publishUserSignup(email)
+
+    return {
+      accessToken: response.AuthenticationResult?.AccessToken,
+      idToken: response.AuthenticationResult?.IdToken,
+      refreshToken: response.AuthenticationResult?.RefreshToken,
+    };
+  }
+   async readQueue(email: string, password: string) {
    return 'hi'
   }
+  async  publishUserSignup(email:string){
+    const message={
+      eventType:"USER_SIGNEDUP",
+      email,
+      createdAt:new Date().toISOString()
+
+  }
+   await sns.send(
+    new PublishCommand({TopicArn:topic,Message:JSON.stringify(message)})
+   )
+   console.log("event published to sns")
+}
+async pollMessage(){
+  const response=await sqs.send(
+    new ReceiveMessageCommand({
+      QueueUrl:queue,
+      MaxNumberOfMessages:1,
+      WaitTimeSeconds:20
+    })
+  )
+  const message=response.Messages||[]
+  for (const msg of message){
+    try {
+      throw  new Error("failed")
+      // console.log("actual msg ",msg.Body)
+      // console.log("body",msg.Body)
+      // const body=JSON.parse(msg.Body!)
+      // const messages=JSON.parse(body.Message)
+      // // console.log("actual msg",message)
+      // // console.log("sending email to",messages.email)
+      // await sqs.send(
+      //   new DeleteMessageCommand({
+      //     QueueUrl:queue,
+      //     ReceiptHandle:msg.ReceiptHandle
+      //   })
+      // )
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
 }
