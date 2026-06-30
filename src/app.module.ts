69a2cfb582e4e513de39d9c2decd528ac9982a86
Arya Gopal <arya@Aryas-MacBook-Air.local>
2026-06-30 11:04:02 -0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 console.log(process.env.DB_USERNAME,"dfghjk")
 import dotenv from 'dotenv'
+import { User } from './user/user.entity';
 dotenv.config();
 
 @Module({
@@ -11,10 +12,13 @@ dotenv.config();
    TypeOrmModule.forRoot({
   type: 'postgres',
   host: process.env.DB_HOST,
-  port: 5432,
+    entities: [User],
+
+  port: 5433,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  synchronize: true,
   ssl: {
     rejectUnauthorized: false,
   },
