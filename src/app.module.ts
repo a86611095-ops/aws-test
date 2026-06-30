155890db3cbe23aba0d16dc272ac9c3db9cac91b
Arya Gopal <arya@Aryas-MacBook-Air.local>
2026-06-30 04:55:21 -0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,18 +9,16 @@ dotenv.config();
 @Module({
    imports: [AuthModule, UserModule,
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      autoLoadEntities: true,
-      ssl: {
-    rejectUnauthorized: false,
-  },
-     synchronize: true, 
-    }),
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  autoLoadEntities: true,
+  synchronize: true,
+  ssl: false,
+}),
     ],
 })
 export class AppModule {}
